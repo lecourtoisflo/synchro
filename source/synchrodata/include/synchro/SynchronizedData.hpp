2fc49a7f96454cb4cfd18e3e6c8dba10d9706434
Le Courtois Florent <florent.lecourtois@rte-france.com>
2021-08-24 09:52:02 +0200

    Send pending signals when all required are received

--- source/synchrodata/include/synchro/SynchronizedData.hpp
+++ source/synchrodata/include/synchro/SynchronizedData.hpp
@@ -68,13 +68,17 @@ public:
     {
         if constexpr (Contains<T, typename R::TupleType>())
         {
-            std::get<std::shared_ptr<T>>(requiredData_) = data;
+            std::get<std::shared_ptr<T>>(requiredData_) = data; // required for following test
             if (areAllRequiredPresent())
             {
                 std::get<Broadcaster<T>>(requiredBroadcasters_).send(data);
                 std::get<std::shared_ptr<T>>(requiredData_).reset();
 
-                // TODO send signals waiting
+                // send pending signals
+                sendSignalsRequired(requiredBroadcasters_, requiredData_);
+                sendSignalsOptional(optionalBroadcasters_, optionalData_);
+                sendSignalsList(listBroadcasters_, listData_);
+
                 // Clear
                 clearAlldata();
                 initDone_ = true;
@@ -87,16 +91,16 @@ public:
                 std::get<Broadcaster<T>>(optionalBroadcasters_).send(data);
                 return;
             }
-            std::get<std::shared_ptr<T>>(optionalData_) = data;
+            std::get<std::shared_ptr<T>>(optionalData_) = data; // put in pending
         }
         else if constexpr (Contains<T, typename L::TupleType>())
         {
             if (initDone_)
             {
-                std::get<Broadcaster<T>>(requiredBroadcasters_).send(data);
+                std::get<Broadcaster<T>>(listBroadcasters_).send(data);
                 return;
             }
-            std::get<std::list<std::shared_ptr<T>>>(listData_).push_back(data);
+            std::get<std::list<std::shared_ptr<T>>>(listData_).push_back(data); // put in pending
         }
     }
 
@@ -126,6 +130,13 @@ private:
         using type    = std::tuple<element<Ts>...>;
     };
 
+    template<class T>
+    using BroadcastersTuple = typename Broadcasters<typename T::TupleType>::type;
+    template<class T>
+    using DataTuple = typename Data<typename T::TupleType>::type;
+    template<class T>
+    using DataListTuple = typename DataList<typename T::TupleType>::type;
+
 private:
     bool areAllRequiredPresent() const
     {
@@ -140,16 +151,87 @@ private:
         std::apply(clear, optionalData_);
         std::apply([](auto&... list) { (..., list.clear()); }, listData_);
     }
+    template<class T>
+    static void sendSignal(Broadcaster<T>& sender, const std::shared_ptr<T>& data)
+    {
+        if (data) { sender.send(data); }
+    }
+
+    template<class... Ts>
+    void sendSignalsRequired(std::tuple<Broadcaster<Ts>...>&,
+                             const std::tuple<std::shared_ptr<Ts>...>&)
+    {
+        static_assert(std::tuple_size_v<std::tuple<Broadcaster<Ts>...>> ==
+                      std::tuple_size_v<std::tuple<std::shared_ptr<Ts>...>>);
+        sendSignalsRequiredImpl<std::tuple_size_v<std::tuple<Broadcaster<Ts>...>>>();
+    }
+
+    template<size_t I, std::enable_if_t<(I > 0), bool> = true>
+    void sendSignalsRequiredImpl()
+    {
+        sendSignal(std::get<I - 1>(requiredBroadcasters_), std::get<I - 1>(requiredData_));
+        sendSignalsRequiredImpl<I - 1>();
+    }
+
+    template<size_t I, std::enable_if_t<(I == 0), bool> = true>
+    void sendSignalsRequiredImpl()
+    {
+    }
+
+    template<class... Ts>
+    void sendSignalsOptional(std::tuple<Broadcaster<Ts>...>&,
+                             const std::tuple<std::shared_ptr<Ts>...>&)
+    {
+        static_assert(std::tuple_size_v<std::tuple<Broadcaster<Ts>...>> ==
+                      std::tuple_size_v<std::tuple<std::shared_ptr<Ts>...>>);
+        sendSignalsOptionalImpl<std::tuple_size_v<std::tuple<Broadcaster<Ts>...>>>();
+    }
+
+    template<size_t I, std::enable_if_t<(I > 0), bool> = true>
+    void sendSignalsOptionalImpl()
+    {
+        sendSignal(std::get<I - 1>(optionalBroadcasters_), std::get<I - 1>(optionalData_));
+        sendSignalsOptionalImpl<I - 1>();
+    }
+
+    template<size_t I, std::enable_if_t<(I == 0), bool> = true>
+    void sendSignalsOptionalImpl()
+    {
+    }
+
+    template<class... Ts>
+    void sendSignalsList(std::tuple<Broadcaster<Ts>...>&,
+                         const std::tuple<std::list<std::shared_ptr<Ts>>...>&)
+    {
+        static_assert(std::tuple_size_v<std::tuple<Broadcaster<Ts>...>> ==
+                      std::tuple_size_v<std::tuple<std::list<std::shared_ptr<Ts>>...>>);
+        sendSignalsListImpl<std::tuple_size_v<std::tuple<Broadcaster<Ts>...>>>();
+    }
+
+    template<size_t I, std::enable_if_t<(I > 0), bool> = true>
+    void sendSignalsListImpl()
+    {
+        for (const auto& data : std::get<I - 1>(listData_))
+        {
+            sendSignal(std::get<I - 1>(listBroadcasters_), data);
+        }
+        sendSignalsListImpl<I - 1>();
+    }
+
+    template<size_t I, std::enable_if_t<(I == 0), bool> = true>
+    void sendSignalsListImpl()
+    {
+    }
 
 private:
     bool initDone_ = false;
-    typename Broadcasters<typename R::TupleType>::type requiredBroadcasters_;
-    typename Broadcasters<typename O::TupleType>::type optionalBroadcasters_;
-    typename Broadcasters<typename L::TupleType>::type listBroadcasters_;
+    BroadcastersTuple<R> requiredBroadcasters_;
+    BroadcastersTuple<O> optionalBroadcasters_;
+    BroadcastersTuple<L> listBroadcasters_;
 
-    typename Data<typename R::TupleType>::type requiredData_;
-    typename Data<typename O::TupleType>::type optionalData_;
-    typename DataList<typename L::TupleType>::type listData_;
+    DataTuple<R> requiredData_;
+    DataTuple<O> optionalData_;
+    DataListTuple<L> listData_;
 };
 
 } // namespace synchro
--- tests/synchrodata/SynchrodataTest.cpp
+++ tests/synchrodata/SynchrodataTest.cpp
@@ -31,27 +31,28 @@ TEST(synchrodata, broadcast)
     ASSERT_EQ(counter, 1);
 }
 
+struct R1
+{
+};
+struct R2
+{
+};
+struct O1
+{
+};
+struct O2
+{
+};
+struct L1
+{
+};
+struct L2
+{
+};
+
 TEST(synchrodata, data)
 {
-    struct R1
-    {
-    };
-    struct R2
-    {
-    };
-    struct O1
-    {
-    };
-    struct O2
-    {
-    };
-    struct L1
-    {
-    };
-    struct L2
-    {
-    };
-    SynchronizedData<Required<R1>, Optional<O1, O2>, List<L1, L2>> data;
+    SynchronizedData<Required<R1>, Optional<O1>> data;
     bool received     = false;
     bool received_opt = false;
     auto connection =
@@ -64,4 +65,48 @@ TEST(synchrodata, data)
 
     data.send<O1>(std::make_shared<O1>());
     ASSERT_TRUE(received_opt);
+}
+
+TEST(synchrodata, waitingData)
+{
+    SynchronizedData<Required<R1, R2>, Optional<O1, O2>, List<L1>> data;
+    bool received              = false;
+    bool received2             = false;
+    bool received_opt          = false;
+    bool received_opt2         = false;
+    unsigned int received_list = 0;
+    auto connection =
+        data.onReceived<R1>([&received](const std::shared_ptr<R1>&) { received = true; });
+    auto connection2 =
+        data.onReceived<R2>([&received2](const std::shared_ptr<R2>&) { received2 = true; });
+
+    auto connection_opt =
+        data.onReceived<O1>([&received_opt](const std::shared_ptr<O1>&) { received_opt = true; });
+    auto connection_opt2 =
+        data.onReceived<O2>([&received_opt2](const std::shared_ptr<O2>&) { received_opt2 = true; });
+
+    auto connection_list =
+        data.onReceived<L1>([&received_list](const std::shared_ptr<L1>&) { ++received_list; });
+
+    data.send<O1>(std::make_shared<O1>());
+    ASSERT_FALSE(received_opt); // pending
+    data.send<L1>(std::make_shared<L1>());
+    data.send<L1>(std::make_shared<L1>());
+    ASSERT_EQ(received_list, 0); // pending
+
+    data.send<R1>(std::make_shared<R1>());
+    ASSERT_FALSE(received);
+    ASSERT_FALSE(received2);
+    ASSERT_FALSE(received_opt);
+    ASSERT_EQ(received_list, 0);
+    data.send<R2>(std::make_shared<R2>());
+    ASSERT_TRUE(received);
+    ASSERT_TRUE(received2);
+    ASSERT_TRUE(received_opt);
+    ASSERT_EQ(received_list, 2);
+    data.send<L1>(std::make_shared<L1>());
+    ASSERT_EQ(received_list, 3);
+
+    data.send<O2>(std::make_shared<O2>());
+    ASSERT_TRUE(received_opt2);
 }
