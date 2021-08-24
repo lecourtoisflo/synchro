a9b7e6c87d24f4241eea4349487a48d4924d3165
Le Courtois Florent <florent.lecourtois@rte-france.com>
2021-08-24 10:59:16 +0200

    Correct naming and formatting

--- source/synchrodata/include/synchro/SynchronizedData.hpp
+++ source/synchrodata/include/synchro/SynchronizedData.hpp
@@ -35,7 +35,7 @@ struct List
 /**
  * @brief Synchronized data
  *
- * Synchronized data allows to synchronize notifications when receiving data according to if
+ * Synchronized data allows to synchronize notifications when receiving data according to whether
  * required data are received:
  * - R : required types (based on @a Required trait). Synchronized data contains 1 element by
  * required type. No notification is sent unless at least one required data of each type is
@@ -69,18 +69,15 @@ public:
     {
         if constexpr (Contains<T, typename R::TupleType>())
         {
-            return std::get<Broadcaster<T>>(requiredBroadcasters_)
-                .onReceived(std::forward<typename Broadcaster<T>::Callback>(cbk));
+            return std::get<Broadcaster<T>>(requiredBroadcasters_).onReceived(std::forward<typename Broadcaster<T>::Callback>(cbk));
         }
         else if constexpr (Contains<T, typename O::TupleType>())
         {
-            return std::get<Broadcaster<T>>(optionalBroadcasters_)
-                .onReceived(std::forward<typename Broadcaster<T>::Callback>(cbk));
+            return std::get<Broadcaster<T>>(optionalBroadcasters_).onReceived(std::forward<typename Broadcaster<T>::Callback>(cbk));
         }
         else if constexpr (Contains<T, typename L::TupleType>())
         {
-            return std::get<Broadcaster<T>>(listBroadcasters_)
-                .onReceived(std::forward<typename Broadcaster<T>::Callback>(cbk));
+            return std::get<Broadcaster<T>>(listBroadcasters_).onReceived(std::forward<typename Broadcaster<T>::Callback>(cbk));
         }
         return Connection<T>();
     }
@@ -97,16 +94,16 @@ public:
     {
         if constexpr (Contains<T, typename R::TupleType>())
         {
-            std::get<std::shared_ptr<T>>(requiredData_) = data; // required for following test
+            std::get<std::shared_ptr<T>>(requiredPendingData_) = data; // required for following test
             if (initDone_ || areAllRequiredPresent())
             {
                 std::get<Broadcaster<T>>(requiredBroadcasters_).send(data);
-                std::get<std::shared_ptr<T>>(requiredData_).reset();
+                std::get<std::shared_ptr<T>>(requiredPendingData_).reset();
 
                 // send pending signals
-                sendSignalsRequired(requiredBroadcasters_, requiredData_);
-                sendSignalsOptional(optionalBroadcasters_, optionalData_);
-                sendSignalsList(listBroadcasters_, listData_);
+                sendSignalsRequired(requiredBroadcasters_, requiredPendingData_);
+                sendSignalsOptional(optionalBroadcasters_, optionalPendingData_);
+                sendSignalsList(listBroadcasters_, listPendingData_);
 
                 // Clear
                 clearAlldata();
@@ -120,7 +117,7 @@ public:
                 std::get<Broadcaster<T>>(optionalBroadcasters_).send(data);
                 return;
             }
-            std::get<std::shared_ptr<T>>(optionalData_) = data; // put in pending
+            std::get<std::shared_ptr<T>>(optionalPendingData_) = data; // put in pending
         }
         else if constexpr (Contains<T, typename L::TupleType>())
         {
@@ -129,7 +126,7 @@ public:
                 std::get<Broadcaster<T>>(listBroadcasters_).send(data);
                 return;
             }
-            std::get<std::list<std::shared_ptr<T>>>(listData_).push_back(data); // put in pending
+            std::get<std::list<std::shared_ptr<T>>>(listPendingData_).push_back(data); // put in pending
         }
     }
 
@@ -142,23 +139,31 @@ private:
     struct Contains<T, std::tuple<Ts...>> : std::disjunction<std::is_same<T, Ts>...>
     {
     };
+
+    /// @brief Broadcasters trait class to define tuple of broadcasters
     template<class T>
     struct Broadcasters;
+    /// @brief Specialization to define a tuple of broadcaster<T> from tuple of T
     template<class... Ts>
     struct Broadcasters<std::tuple<Ts...>>
     {
         using type = std::tuple<Broadcaster<Ts>...>;
     };
+
+    /// @brief Data trait class to define tuple of elements
     template<class T>
     struct Data;
+    /// @brief Specialization to define a tuple of shared pointers from a tuple
     template<class... Ts>
     struct Data<std::tuple<Ts...>>
     {
         using type = std::tuple<std::shared_ptr<Ts>...>;
     };
 
+    /// @brief Data trait class to define tuple of lists of elements
     template<class T>
     struct DataList;
+    /// @brief Specialization to define a tuple of lists of shared pointers from a tuple
     template<class... Ts>
     struct DataList<std::tuple<Ts...>>
     {
@@ -177,16 +182,15 @@ private:
 private:
     bool areAllRequiredPresent() const
     {
-        return std::apply([](auto&... ptr) { return (... && (ptr.use_count() > 0)); },
-                          requiredData_);
+        return std::apply([](auto&... ptr) { return (... && (ptr.use_count() > 0)); }, requiredPendingData_);
     }
 
     void clearAlldata()
     {
         auto clear = [](auto&... ptr) { (..., ptr.reset()); };
-        std::apply(clear, requiredData_);
-        std::apply(clear, optionalData_);
-        std::apply([](auto&... list) { (..., list.clear()); }, listData_);
+        std::apply(clear, requiredPendingData_);
+        std::apply(clear, optionalPendingData_);
+        std::apply([](auto&... list) { (..., list.clear()); }, listPendingData_);
     }
     template<class T>
     static void sendSignal(Broadcaster<T>& sender, const std::shared_ptr<T>& data)
@@ -195,18 +199,16 @@ private:
     }
 
     template<class... Ts>
-    void sendSignalsRequired(std::tuple<Broadcaster<Ts>...>&,
-                             const std::tuple<std::shared_ptr<Ts>...>&)
+    void sendSignalsRequired(std::tuple<Broadcaster<Ts>...>&, const std::tuple<std::shared_ptr<Ts>...>&)
     {
-        static_assert(std::tuple_size_v<std::tuple<Broadcaster<Ts>...>> ==
-                      std::tuple_size_v<std::tuple<std::shared_ptr<Ts>...>>);
+        static_assert(std::tuple_size_v<std::tuple<Broadcaster<Ts>...>> == std::tuple_size_v<std::tuple<std::shared_ptr<Ts>...>>);
         sendSignalsRequiredImpl<std::tuple_size_v<std::tuple<Broadcaster<Ts>...>>>();
     }
 
     template<size_t I, std::enable_if_t<(I > 0), bool> = true>
     void sendSignalsRequiredImpl()
     {
-        sendSignal(std::get<I - 1>(requiredBroadcasters_), std::get<I - 1>(requiredData_));
+        sendSignal(std::get<I - 1>(requiredBroadcasters_), std::get<I - 1>(requiredPendingData_));
         sendSignalsRequiredImpl<I - 1>();
     }
 
@@ -216,18 +218,16 @@ private:
     }
 
     template<class... Ts>
-    void sendSignalsOptional(std::tuple<Broadcaster<Ts>...>&,
-                             const std::tuple<std::shared_ptr<Ts>...>&)
+    void sendSignalsOptional(std::tuple<Broadcaster<Ts>...>&, const std::tuple<std::shared_ptr<Ts>...>&)
     {
-        static_assert(std::tuple_size_v<std::tuple<Broadcaster<Ts>...>> ==
-                      std::tuple_size_v<std::tuple<std::shared_ptr<Ts>...>>);
+        static_assert(std::tuple_size_v<std::tuple<Broadcaster<Ts>...>> == std::tuple_size_v<std::tuple<std::shared_ptr<Ts>...>>);
         sendSignalsOptionalImpl<std::tuple_size_v<std::tuple<Broadcaster<Ts>...>>>();
     }
 
     template<size_t I, std::enable_if_t<(I > 0), bool> = true>
     void sendSignalsOptionalImpl()
     {
-        sendSignal(std::get<I - 1>(optionalBroadcasters_), std::get<I - 1>(optionalData_));
+        sendSignal(std::get<I - 1>(optionalBroadcasters_), std::get<I - 1>(optionalPendingData_));
         sendSignalsOptionalImpl<I - 1>();
     }
 
@@ -237,18 +237,16 @@ private:
     }
 
     template<class... Ts>
-    void sendSignalsList(std::tuple<Broadcaster<Ts>...>&,
-                         const std::tuple<std::list<std::shared_ptr<Ts>>...>&)
+    void sendSignalsList(std::tuple<Broadcaster<Ts>...>&, const std::tuple<std::list<std::shared_ptr<Ts>>...>&)
     {
-        static_assert(std::tuple_size_v<std::tuple<Broadcaster<Ts>...>> ==
-                      std::tuple_size_v<std::tuple<std::list<std::shared_ptr<Ts>>...>>);
+        static_assert(std::tuple_size_v<std::tuple<Broadcaster<Ts>...>> == std::tuple_size_v<std::tuple<std::list<std::shared_ptr<Ts>>...>>);
         sendSignalsListImpl<std::tuple_size_v<std::tuple<Broadcaster<Ts>...>>>();
     }
 
     template<size_t I, std::enable_if_t<(I > 0), bool> = true>
     void sendSignalsListImpl()
     {
-        for (const auto& data : std::get<I - 1>(listData_))
+        for (const auto& data : std::get<I - 1>(listPendingData_))
         {
             sendSignal(std::get<I - 1>(listBroadcasters_), data);
         }
@@ -266,9 +264,9 @@ private:
     BroadcastersTuple<O> optionalBroadcasters_;
     BroadcastersTuple<L> listBroadcasters_;
 
-    DataTuple<R> requiredData_;
-    DataTuple<O> optionalData_;
-    DataListTuple<L> listData_;
+    DataTuple<R> requiredPendingData_;
+    DataTuple<O> optionalPendingData_;
+    DataListTuple<L> listPendingData_;
 };
 
 } // namespace synchro
